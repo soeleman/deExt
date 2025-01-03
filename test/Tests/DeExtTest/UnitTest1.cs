9bdd0e6c5ac051e0ea00390c2685badc46fb7e36
dede <soeleman@gmail.com>
2025-01-03 22:22:17 +0700

    Add Test Solution

---
+++ test/Tests/DeExtTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using DeExt;
+namespace DeExtTest
+{
+    public class StringUnitTest
+    {
+        [Theory]
+        [InlineData(true, null)]
+        [InlineData(true, "")]
+        [InlineData(false, "One")]
+        public void Base(bool state, string value)
+        {
+            if (state)
+            {
+                Assert.True(value.IsNullOrWhiteSpace());
+            }
+            else
+            {
+                Assert.False(value.IsNullOrWhiteSpace());
+            }
+        }
+    }
+}
